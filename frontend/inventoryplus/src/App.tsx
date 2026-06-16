7f79431f0435be5bd97fb9475dbfeedc869a88ed
bonteves43 <bonteves43@gmail.com>
2026-06-16 13:18:47 +0800

    Created a ProfileCard

--- frontend/inventoryplus/src/App.tsx
+++ frontend/inventoryplus/src/App.tsx
@@ -1,4 +1,4 @@
-import Card from './components/card'
+import ProfileCard from './components/profileCard'
 import { SidebarNavigationSlimDemo } from './components/sidebar'
 import './App.css'
 
@@ -7,16 +7,40 @@ function App() {
     <div className="app-layout">
       <SidebarNavigationSlimDemo />
       <main className="app-main">
-        <Card
-          title="Product Name"
-          category="Category"
-          quantity={0}
-          price={0.00}
-          sku="SKU-0000"
-          status="in-stock"
-          onEdit={() => console.log('edit')}
-          onDelete={() => console.log('delete')}
+        <ProfileCard
+          name="EDDT"
+          title="EHANCE ARMAMENT"
+          handle="javicodes"
+          status="Online"
+          contactText="Contact Me"
+          avatarUrl="/path/to/avatar.jpg"
+          showUserInfo={false}
+          enableTilt={true}
+          enableMobileTilt={false}
+          onContactClick={() => console.log('Contact clicked')}
+          behindGlowColor="rgba(125, 190, 255, 0.67)"
+          iconUrl="/assets/demo/iconpattern.png"
+          behindGlowEnabled
+          innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
         />
+        <ProfileCard
+          name="EDDT"
+          title="EHANCE ARMAMENT"
+          handle="javicodes"
+          status="Online"
+          contactText="Contact Me"
+          avatarUrl="/path/to/avatar.jpg"
+          showUserInfo={false}
+          enableTilt={true}
+          enableMobileTilt={false}
+          onContactClick={() => console.log('Contact clicked')}
+          behindGlowColor="rgba(125, 190, 255, 0.67)"
+          iconUrl="/assets/demo/iconpattern.png"
+          behindGlowEnabled
+          innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
+        />
+
+
       </main>
     </div>
   )
